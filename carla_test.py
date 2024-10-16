df0e15c3848c4db19881ace01a59fac210e33490
Martin Lilly <mlilly@oswego.edu>
2024-10-15 23:06:13 -0400

    Update carla_test.py

--- carla_test.py
+++ carla_test.py
@@ -20,7 +20,7 @@ import random
 from time import sleep
 import time
 try:
-    sys.path.append(glob.glob('../carla-*%d.%d-%s.egg' % (
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
         sys.version_info.major,
         sys.version_info.minor,
         'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
