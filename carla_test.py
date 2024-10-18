48bb1dcf3316d932994202fd80fe37f6d1c2d15e
Martin Lilly <mlilly@oswego.edu>
2024-10-18 08:23:11 -0400

    Update carla_test.py

--- carla_test.py
+++ carla_test.py
@@ -5,7 +5,7 @@ import random
 from time import sleep
 import time
 try:
-    sys.path.append(glob.glob('C:/Users/mjlil/OneDrive/Documents/Carla/WindowsNoEditor/PythonAPI/carla/dist/carla-*%d.%d-%s.egg' % (
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
         sys.version_info.major,
         sys.version_info.minor,
         'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
