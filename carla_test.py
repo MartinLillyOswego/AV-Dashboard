89a383c55b07ba83e650858a39c76ff28d2f6635
Mlilly8 <mlilly@oswego.edu>
2024-10-17 21:05:48 -0400

    Update carla_test.py
    Added steering angle output

--- carla_test.py
+++ carla_test.py
@@ -1,18 +1,3 @@
-"""
-!!WARNING!!
-Due to this project using Carla 9.8 this script requires Python 3.7 in order to work with the Carla host and the Carla python package.
-Once Python 3.7 is installed use pip to install the Carla package "pip install carla==0.9.5" (this command may result in errors but does not affect performance)
-!!WARNING!!
-
-This is a test script that will spawn a Tesla Model 3 into Carla with autopilot enabled. The vhicle seep will be printed every .025 seconds for 30 seconds then the car will
-be destoyed.
-
-To use the script download Carla 9.8 and navigate to "[Carla folder]/WindowsNoEditor/PythonAPI/examples/" and place this file into this directory.
-Once Carla is running simply run the script.
-Since the script is being run on the same computer you won't need to change the IP address, "localhost" will work.
-
-Note: Carla simulator 9.8 requires at least 4GB of dedicated video memory to run properly.
-"""
 import glob
 import os
 import sys
@@ -20,7 +5,7 @@ import random
 from time import sleep
 import time
 try:
-    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+    sys.path.append(glob.glob('C:/Users/mjlil/OneDrive/Documents/Carla/WindowsNoEditor/PythonAPI/carla/dist/carla-*%d.%d-%s.egg' % (
         sys.version_info.major,
         sys.version_info.minor,
         'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
@@ -38,7 +23,7 @@ try:
     world = client.get_world()
     blueprint_library = world.get_blueprint_library()
 
-    # Create Modle 3 Blueprint
+    # Create Model 3 Blueprint
     bp = blueprint_library.filter("model3")[0]
     print(bp)
 
@@ -53,21 +38,35 @@ try:
 
     vehicle.set_autopilot(True)
 
-    # This is the loop that will grab telementry data and send it to the dashboard
+    # This is the loop that will grab telemetry data and send it to the dashboard
     running = True
-    start = time.time() # Start timer for runtime
+    start = time.time()
     while running:
         end = time.time()
-        if (end - start) >= 30: running = False # Stop loop after 30 seconds
-        # Get the velocity vector of the vehicle
-        velocity = vehicle.get_velocity()
+        if (end - start) >= 30:
+            running = False
+        try:
+            # Get the velocity vector of the vehicle and calculate speed
+            velocity = vehicle.get_velocity()
+            speed = 2.237 * (velocity.x**2 + velocity.y**2 + velocity.z**2)**0.5  # Speed in mph
 
-        # Convert velocity to speed (magnitude)
-        Covervion_factor = 2.237 # Use 2.237 for MPH and 3.6 for KPH
-        speed = Covervion_factor * (velocity.x**2 + velocity.y**2 + velocity.z**2)**0.5
-        
-        print(f"Speed: {speed:.2f} MPH")
-        sleep(.025)
+            # Get the vehicle control for steering angle
+            control = vehicle.get_control()
+            physics_control = vehicle.get_physics_control()
+
+            max_steer_angle = max(wheel.max_steer_angle for wheel in physics_control.wheels)
+            
+            # max_steer_angle = physics_control.wheels.WheelPhysicsControl(max_steer_angle)
+            steering_angle = control.steer * max_steer_angle  # Approximating to degrees, -30 to +30 range
+
+            print(f"Speed: {speed:.2f} mph | Steering Angle: {steering_angle:.2f} degrees")
+            # print(f"Steering Angle: {steering_angle:.2f} degrees")
+            # print(f"Wheel Speeds: {[f'{ws:.2f} mph' for ws in wheel_speeds]}")
+
+        except Exception as e:
+            print(f"Could not retrieve telemetry data: {e}")
+
+        sleep(1)
 
 # Destroy all actors before quitting
 finally:
