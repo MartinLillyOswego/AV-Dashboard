b9c0ac413175548f37cdbeaaea31a5d86e07824e
AlekWhite <63272278+AlekWhite@users.noreply.github.com>
2024-10-22 18:41:15 -0400

    Update config.py

--- WindowsNoEditor/PythonAPI/examples/config.py
+++ WindowsNoEditor/PythonAPI/examples/config.py
@@ -16,7 +16,7 @@ SEND_INTERVAL = .25  # total wait time elapsed between sending packets
 PACKET_COUNT = 60  # number of packets the Vehicle class will store at any given time
 MY_ID = 0  # define the Sender ID to be the final string of digits of the sending machine’s IP address
 EXTERNAL_ID = 1  # define the Receiver ID to be the final string of digits in the receiving machine’s IP address
-PACKET_SIZE = 136  # 17 floats in each half = 2(17*4) = 136 bytes
+PACKET_SIZE = 136  # 17 floats in each half = 2(17*4) = 136 bytes (~56bytes max)
 
 CARLA_SIM_DURATION = 30  # number of seconds that the carla simulation will run for
 
