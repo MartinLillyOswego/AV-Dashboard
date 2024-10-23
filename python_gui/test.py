feb91752bd2f5efb6d830aaf7d2140600060c603
Martin Lilly <mlilly@oswego.edu>
2024-10-22 20:11:44 -0400

    Add files via upload

---
+++ python_gui/test.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import uvicorn
+import random
+
+app = FastAPI()
+current_speed = 1
+direction = "f"
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_index():
+    with open("index.html", 'r') as f:
+        return HTMLResponse(content=f.read())
+
+@app.get("/speed")
+async def get_speed():
+    global direction
+    global current_speed
+    if direction == "f":
+        current_speed = current_speed + 1
+        if current_speed == 100: direction = "b"
+    else:
+        current_speed = current_speed - 1
+        if current_speed == 1: direction = "f"
+    return {"speed": current_speed}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
