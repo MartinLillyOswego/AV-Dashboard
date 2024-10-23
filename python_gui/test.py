8390657a1c5d5f8818796dbadf629fb9735a910f
CaveRaider <NoahBoutwell1@gmail.com>
2024-10-23 00:20:53 -0400

    Icon fix

--- python_gui/test.py
+++ python_gui/test.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 import random
 
 app = FastAPI()
 current_speed = 1
 direction = "f"
+
+app.mount("/icons", StaticFiles(directory="icons"), name="icons")
 
 
 @app.get("/", response_class=HTMLResponse)
