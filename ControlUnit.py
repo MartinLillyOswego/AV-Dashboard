9dfaf43d0f6657fc8c1416fff5d0bcadd0c53bd7
CaveRaider <nboutwel@oswego.edu>
2024-10-30 16:09:49 -0400

    gui brokey

--- ControlUnit.py
+++ ControlUnit.py
@@ -21,6 +21,7 @@ class ControlUnit(threading.Thread):
         # generate all required values and assets neeed to fully update the display
         # display_data + self.generate_vehicle_error() + otherStuff
         # self.vehicle.update_calculated_data(display_data)
+        pass
 
     # called when generating response commands, responsible for observing vehicle errors
     def generate_vehicle_error(self):
