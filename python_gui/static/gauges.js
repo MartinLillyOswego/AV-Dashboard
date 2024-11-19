d98a0bb8a825afa9fab28573e9c4d921e0593d63
Mlilly8 <mlilly@oswego.edu>
2024-11-19 15:24:23 -0500

    Changed speedometer JS code to class

--- python_gui/static/gauges.js
+++ python_gui/static/gauges.js
@@ -1,96 +1,57 @@
 //==================================Speedometer========================================
-function resize (container1, container2) {
-		const con1 = document.getElementById(container1);
-		const con2 = document.getElementById(container2);
-		const con1Width = con1.offsetWidth;
+class speedometer {
+	constructor (maxSpeed, container1, container2) {
+		// Set max speed to a multiple of 10
+		while ((maxSpeed % 10) !== 0) {maxSpeed++;}
+		// Get div containers
+		this.scalesContainer = document.getElementById('scales-container');
+		this.speedValues = document.getElementById('speed-values');
+		this.container1 = document.getElementById(container1);
+		this.container2 = document.getElementById(container2);
+		this.speedDisplay = document.getElementById("value");
+		this.arrow = document.getElementById("arrow-wrapper");
+		// Build class variables 
+		this.maxSpeed = maxSpeed + 10; //Add 10 to max speed for padding on the speedometer(optional)
+		this.numScales = (this.maxSpeed / 5) + 1;
+		this.speedPerAngle = this.maxSpeed / 230;
+		this.angleGap = 5 / this.speedPerAngle;
+		this.anglePerSpeed = 230 / this.maxSpeed
+		// Initalize speedometer
+		this.buildScales()
+		this.resize()
+	}
+
+	resize() {
+		const con1Width = this.container1.offsetWidth;
 		
-		con1.style.height = con1Width * 0.72 + "px"
-		con2.style.height = con1Width * 0.72 + "px"
+		this.container1.style.height = con1Width * 0.72 + "px"
+		this.container2.style.height = con1Width * 0.72 + "px"
 	}
 
-function setSpeed (maxSpeed, speed){
-	while ((maxSpeed % 5) !== 0) {maxSpeed++;}
-
-	maxSpeed = maxSpeed + 10;
-	anglePerSpeed = 230 / maxSpeed
-
-	document.getElementById("value").innerHTML = speed;
-	if (speed > maxSpeed) {speed = maxSpeed}
-	document.getElementById("arrow-wrapper").style.transform = "rotate("+ ((speed * anglePerSpeed) - 25) + "deg)";
-}
-
-function initalizeSpeedometer(maxSpeed, container1, container2) {
-
-	resize('speedometer-container-1', 'speedometer-container-2')
-
-	while ((maxSpeed % 5) !== 0) {maxSpeed++;}
-
-	scalesContainer = document.getElementById('scales-container')
-	speedValues = document.getElementById('speed-values')
-	maxSpeed = maxSpeed + 10;
-	numScales = (maxSpeed / 5) + 1;
-	speedPerAngle = maxSpeed / 230;
-	angleGap = 5 / speedPerAngle;
-	htmlString = "";
-	htmlString2 = "";
-
-	for (i=0; i < numScales; i++) {
-		if (((i % 2) === 1) && (numScales > 15)) {
-			htmlString = htmlString + "<div class='half-scale-container scale-" + i + "' style='transform: rotate(" +((angleGap * i) - 115)+ "deg);'><div class='scales'></div></div>"
-		}
-		else {
-			
-			htmlString = htmlString + "<div class='scale-container scale-" + i + "' style='transform: rotate(" +((angleGap * i) - 115)+ "deg);'><div class='scales'></div></div>"
-			htmlString2 = htmlString2 + "<div class='scale-values speed-scale-" + i + "' style='transform: rotate(" +((angleGap * i) - 115)+ "deg);'><div class='speed-scale-value-top' style=' transform: rotate("+ -((angleGap * i) - 115) +"deg);'>"+ (5 * i) +"</div></div>"
-		}
+	setSpeed(speed) {
+		this.speedDisplay.innerHTML = speed;
+		if (speed > this.maxSpeed) {speed = this.maxSpeed}
+		this.arrow.style.transform = "rotate("+ ((speed * this.anglePerSpeed) - 25) + "deg)";
 	}
 
-	scalesContainer.innerHTML = htmlString
-	speedValues.innerHTML = htmlString2
+	buildScales() {
+		var htmlString = "";
+		var htmlString2 = "";
+		for (let i=0; i < this.numScales; i++) {
+			if (((i % 2) === 1) && (this.numScales > 15)) {
+				htmlString = htmlString + "<div class='half-scale-container scale-" + i + "' style='transform: rotate(" +((this.angleGap * i) - 115)+ "deg);'><div class='scales'></div></div>"
+			}
+			else {
+				
+				htmlString = htmlString + "<div class='scale-container scale-" + i + "' style='transform: rotate(" +((this.angleGap * i) - 115)+ "deg);'><div class='scales'></div></div>"
+				htmlString2 = htmlString2 + "<div class='scale-values speed-scale-" + i + "' style='transform: rotate(" +((this.angleGap * i) - 115)+ "deg);'><div class='speed-scale-value-top' style=' transform: rotate("+ -((this.angleGap * i) - 115) +"deg);'>"+ (5 * i) +"</div></div>"
+			}
+		}
 
+		this.scalesContainer.innerHTML = htmlString
+		this.speedValues.innerHTML = htmlString2
+	}
 }
-
-// function initalizeSpeedometer(numScales, container1, container2) {
-
-// 	resize('speedometer-container-1', 'speedometer-container-2')
-
-// 	scalesContainer = document.getElementById('scales-container')
-// 	speedValues = document.getElementById('speed-values')
-// 	htmlString = ""
-// 	htmlString2 = ""
-// 	angleGap = (180/numScales)
-// 	speedsListtemp = []
-// 	speedsList = []
-
-// 	for (i=0; i < numScales; i++){speedsListtemp.push(((180/numScales)*i)); speedsListtemp.push(((180/numScales)*i)-180);}
-// 	for (i=0; i < speedsListtemp.length; i++){if (speedsListtemp[i] > -110 && speedsListtemp[i] < 110){speedsList.push(speedsListtemp[i])}}
-	
-// 	speedsList.sort(function (a, b) { return a - b })
-// 	speedScale = 56 / 230
-
-// 	for (i=0; i < numScales; i++) {
-// 		htmlString = htmlString + "<div class='scales scale-" + i + "' style='transform: rotate(" +((180/numScales)*i)+ "deg);'></div>"
-
-// 		if ((((180/numScales)*i) < 115) && !((((180/numScales)*i)-180) > -110)){
-// 			htmlString2 = htmlString2 + "<div class='scale-values speed-scale-" + i + "' style='transform: rotate(" +((180/numScales)*i)+ "deg);'><div class='speed-scale-value-top' style=' transform: rotate("+ -((180/numScales)*i) +"deg);'>"+ Math.round(((speedsList.indexOf(((180/numScales))*i)*speedScale)*angleGap)) +"</div></div>"
-// 		}
-// 		if (((((180/numScales)*i)-180) > -110) && (((180/numScales)*i) < 115)) {
-// 			htmlString2 = htmlString2 + "<div class='scale-values speed-scale-" + i + "' style='transform: rotate(" +((180/numScales)*i)+ "deg);'><div class='speed-scale-value-top' style=' transform: rotate("+ -((180/numScales)*i) +"deg);'>"+ Math.round(((speedsList.indexOf(((180/numScales))*i)*speedScale)*angleGap)) +"</div><div class='speed-scale-value-bottom' style='transform: rotate("+ -((180/numScales)*i) +"deg);'>"+ Math.round(((speedsList.indexOf(((180/numScales)*i)-180)*speedScale)*angleGap)) +"</div></div>"
-// 		}
-
-// 		if ((((180/numScales)*i) > 115)) {
-// 			htmlString2 = htmlString2 + "<div class='scale-values speed-scale-" + i + "' style='transform: rotate(" +((180/numScales)*i)+ "deg);'><div class='speed-scale-value-bottom' style='transform: rotate("+ -((180/numScales)*i) +"deg);'>"+ Math.round(((speedsList.indexOf(((180/numScales)*i)-180)*speedScale)*angleGap)) +"</div></div>"
-// 		}
-
-// 	}
-
-// 	scalesContainer.innerHTML = htmlString
-// 	speedValues.innerHTML = htmlString2
-// }
-
-function handleResize() {console.log("Window resized!"); }
-
-window.addEventListener("resize", function(){resize('speedometer-container-1', 'speedometer-container-2')});
 
 //==================================Steering Graphic========================================
 function setAngle(angle) {
