d3d28002e5b8a94d62bc14701fb9b76168930fe8
AlekWhite <63272278+AlekWhite@users.noreply.github.com>
2024-11-20 17:09:08 -0500

    Update config.py

--- control/config.py
+++ control/config.py
@@ -8,47 +8,64 @@ import re
 
 # known values
 CONFIG_FILE = "control/config.txt"
-CONTROL_UNIT_INTERVAL = 0.2
 
 # values to be loaded from file
 VEHICLE_PORT = -1  # radio's serial port
+LOCAL_PORT = -1  # emulated serial port
+USE_LOCAL_PORT = False  # run over an emulated serial port, rather than radio
 SEND_INTERVAL = -1  # total wait time elapsed between sending packets
 PACKET_COUNT = -1  # number of packets the Vehicle class will store at any given time
-MY_ID = -1  # define the Sender ID to be the final string of digits of the sending machine’s IP address
-EXTERNAL_ID = -1  # define the Receiver ID to be the final string of digits in the receiving machine’s IP address
-PACKET_SIZE = -1  # 17 floats in each half = 2(17*4) = 136 bytes
 MAX_SPEED = -1
+PACKET_HEADER = b""
 
 # pull values from config.txt
 def read(input_file):
     try:
-        global VEHICLE_PORT, SEND_INTERVAL, PACKET_SIZE, PACKET_COUNT, MY_ID, EXTERNAL_ID, MAX_SPEED
+        global VEHICLE_PORT, SEND_INTERVAL, PACKET_COUNT, MAX_SPEED, PACKET_HEADER, LOCAL_PORT, USE_LOCAL_PORT
         with open(input_file, 'r') as file:
             lines = file.readlines()
         vals = []
         for line in lines:
             vals.append([exp.group() for exp in re.finditer(r": (.{,16})", str(line))][0][2:])
+        print(vals)
         VEHICLE_PORT = str(vals[0])
-        SEND_INTERVAL = float(vals[1])
-        PACKET_COUNT = int(vals[2])
-        MY_ID = int(vals[3])
-        EXTERNAL_ID = int(vals[4])
-        PACKET_SIZE = int(vals[5])
+        LOCAL_PORT = str(vals[1])
+
+        if str(vals[2]) == "True":
+            USE_LOCAL_PORT = True
+        else:
+            USE_LOCAL_PORT = False
+
+        SEND_INTERVAL = float(vals[3])
+        PACKET_COUNT = int(vals[4])
+        PACKET_HEADER = str(vals[5])
+
+        # pull packet header
+        valsH = [exp.group() for exp in re.finditer(r"[0-9]+", str(PACKET_HEADER))]
+        b = b""
+        for val in valsH:
+            b += int(val).to_bytes(1, "big")
+        PACKET_HEADER = b
+
         MAX_SPEED = float(vals[6])
- 
+
     except:
         raise RuntimeError(f"{get_time()}:Config: Failed to read file")
 
 # put values into config.txt
 def write():
     try:
+        header = PACKET_HEADER
+        if type(header) == bytes:
+            header = f"{int(header[0])}.{int(header[1])}.{int(header[2])}"
         new_config = f"""VEHICLE_PORT: {VEHICLE_PORT}
+LOCAL_PORT: {LOCAL_PORT}
+USE_LOCAL_PORT: {USE_LOCAL_PORT}
 SEND_INTERVAL: {SEND_INTERVAL}
 PACKET_COUNT: {PACKET_COUNT} 
-MY_ID: {MY_ID}
-EXTERNAL_ID: {EXTERNAL_ID}
-PACKET_SIZE: {PACKET_SIZE}
-MAX_SPEED: {MAX_SPEED}"""
+PACKET_HEADER: {header}
+MAX_SPEED: {MAX_SPEED}
+"""
         with open(CONFIG_FILE, "w") as file:
             file.write(new_config)
     except:
